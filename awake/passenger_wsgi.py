4fe7214559a8e8ecd7d00e0cc0f2fa0151cefe04
hacker <prycetreasure44@gmail.com>
2025-06-27 16:15:20 +0300

    pass7

---
+++ awake/passenger_wsgi.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+sys.path.insert(0, '/home/awakzfip/public_html/awake')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ecom.settings'
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
--- passenger_wsgi.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-sys.path.insert(0, '/home/awakzfip/public_html')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ecom.settings'  # Use your Django project package name here
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
