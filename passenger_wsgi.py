4bd9dd4853d33fcb44cc68d85d18604de8b8f8d9
hacker <prycetreasure44@gmail.com>
2025-06-27 16:01:38 +0300

    pass5

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.insert(0, '/home/ssebadduka/awakening_saints')
+sys.path.insert(0, '/home/awakzfip/awakening_saints')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ecom.settings'
 
 from django.core.wsgi import get_wsgi_application
