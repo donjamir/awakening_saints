3df6bdcbcaf97df9a958770f136acf4df7942ef4
hacker <prycetreasure44@gmail.com>
2025-06-27 15:15:51 +0300

    Add passenger_wsgi.py for Namecheap deployment

---
+++ passenger_wsgi.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+# Add your project directory to the sys.path
+sys.path.insert(0, '/home/your_cpanel_username/ecommerce_project')
+
+# Set the settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ecom.settings'
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
