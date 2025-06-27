6dfda1d80d32334988147a489b9735bdbb5bd6d6
hacker <prycetreasure44@gmail.com>
2025-06-27 17:52:22 +0300

    pass17

--- awake/ecom/settings.py
+++ awake/ecom/settings.py
@@ -102,7 +102,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 ######## These tell django about the custom user modal we created ########
