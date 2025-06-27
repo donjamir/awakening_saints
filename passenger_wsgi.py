d8b02e1dcd8c275ddeeb003f9816a7bda4b9f2d7
hacker <prycetreasure44@gmail.com>
2025-06-27 16:09:34 +0300

    pass6

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,8 @@
 import sys
 import os
 
-sys.path.insert(0, '/home/awakzfip/awakening_saints')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ecom.settings'
+sys.path.insert(0, '/home/awakzfip/public_html')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ecom.settings'  # Use your Django project package name here
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
