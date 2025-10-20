fce0691bae410569dfe356572347acd920168232
hacker <prycetreasure44@gmail.com>
2025-10-20 12:34:44 +0300

    awake_done20

--- awake/ecom/settings.py
+++ awake/ecom/settings.py
@@ -133,13 +133,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-ev28k=ir#vah2v)!-*t65t!+b#j(xnk24fv88!nkoemy)#0ag1'
 
-# DEBUG = True
+DEBUG = True
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 
-DEBUG = False
+# DEBUG = False
 
-ALLOWED_HOSTS = ['awakeningsaints.org', '*']
+# ALLOWED_HOSTS = ['awakeningsaints.org', '*']
 
 
 
@@ -267,27 +267,27 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ecom.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'awakzfip_awake',
-        'USER': 'awakzfip_kal',
-        'PASSWORD': 'jamir1.022',  # the password you set in cPanel
-        'HOST': 'localhost',              # or the full domain if remote
-        'PORT': '3306',
-    }
-}
-
-
-
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'awakzfip_awake',
+#         'USER': 'awakzfip_kal',
+#         'PASSWORD': 'jamir1.022',  # the password you set in cPanel
+#         'HOST': 'localhost',              # or the full domain if remote
+#         'PORT': '3306',
 #     }
 # }
+
+
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
